230a97c5b7855e862a80fb11029fba76a623ff93
ohbob <esrobertsozolins@gmail.com>
2020-08-10 06:36:30 +0300

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import App from './App.svelte';
 
-var app = new App({
-	target: document.body
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'world'
+	}
 });
 
 export default app;
